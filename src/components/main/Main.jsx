2be2bdb4e1f41e826fa9f85ecafd99751240745b
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-12-02 23:52:06 +0100

    Modifico main, sposto tutto dentro a funzione

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -7,38 +7,41 @@ import { posts } from '../../posts.js'
 //importo useState
 import { useState } from 'react'
 
-//stampo solo post pubblici
-const pubPosts = posts.filter((post) => post.published === true)
-
-// disabilito invio del form
-function addPost(e) {
-    e.preventDefault()
-    console.log('Aggiungi post')
-}
-
-// creo variabile per eliminare spazi nell'input del form
-const newTitle = title.trim()
-
-// input per creare un nuovo post
-const [title, setTitle] = useState('')
-// const [posts, setPosts] = useState('')
-
-// invoco funzione a cui fa aggiunto l'array completo di posts + il nuovo post da form
-setPosts([...posts, newPostTitle])
-
-// creo variabile con oggetto gli elementi che mi servono per creare nuovo post
-const newPostTitle = {
-    id: Date.now(),
-    title: newTitle,
-    image: placeholder,
-    content:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.',
-    tags: ['html', 'css'],
-    published: true,
-}
-
-
 export default function Main() {
+
+    //stampo solo post pubblici
+    const pubPosts = posts.filter((post) => post.published === true)
+
+    // variabili per nuovo post
+    const [title, setTitle] = useState('')
+    const [newPost, setNewPost] = useState(posts)
+
+    function addPost(event) {
+        event.preventDefault()
+
+        const newTitle = title.trim()
+
+        if (newTitle === '') return
+
+        const addPost = {
+            id: Date.now(),
+            title: newTitle,
+            image: undefined,
+            content: 'Nuovo contenuto del post',
+            tags: [],
+            published: true,
+        }
+
+        setNewPost(prevPosts => [...prevPosts, addedPost])
+        console.log('stai aggiungendo un nuovo post')
+
+    }
+
+    // Elimino un post
+    // function deletePost(postId) {
+    //     setNewPost(prevPosts => prevPosts.filter(post => post.id !== postId))
+    // }
+
 
     return (
         <main>
@@ -46,7 +49,7 @@ export default function Main() {
                 <h1 className={style.title_blog}>Il mio blog</h1>
                 <div className={style.container}>
                     <form className={style.form} action="" onSubmit={addPost}>
-                        <input className={style.post_form} type="text" placeholder='Nuovo Post' value={newTitle} onChange={(e) => setTitle(e.target.value)} />
+                        <input className={style.post_form} type="text" placeholder='Nuovo Post' value={title} onChange={(e) => setTitle(e.target.value)} />
                         <input className={style.submit_form} type="submit" value='Aggiungi' />
                     </form>
                     <div className={style.raw}>
